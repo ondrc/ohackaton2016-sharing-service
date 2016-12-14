2fab8135ef6deff04a2f5105afde5584d7836caa
Ondrej Cerny <ondrej.cerny@oracle.com>
2016-12-14 18:27:31 +0100

    Booking service -> registration

--- src/hackathon.2016/sharingservice/registration/RegistrationService.go
+++ src/hackathon.2016/sharingservice/registration/RegistrationService.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	http.HandleFunc(common.REGISTRATION_URI, addItem)
+	http.HandleFunc(common.BOOKING_URI, bookItem)
 	log.Fatal(http.ListenAndServe(common.REGISTRATION_LISTEN_ADDRESS, nil))
 }
 
@@ -58,5 +59,47 @@ func postItem(item common.ItemRegistration) string {
 		return err.Error()
 	} else {
 		return strings.Join(res,",")
+	}
+}
+
+func bookItem(w http.ResponseWriter, r *http.Request) {
+	var bookingInfo common.BookingInfo
+	err := json.NewDecoder(r.Body).Decode(&bookingInfo)
+	if err != nil {
+		fmt.Errorf("Failed to parse request: %v", err)
+	}
+	w.Write([]byte(postInfo(bookingInfo)))
+}
+
+func postInfo(bookingInfo common.BookingInfo) string {
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, common.PROJECT_ID)
+	if err != nil {
+		fmt.Errorf("Failed to create client: %v", err)
+	}
+
+	topic, err := client.CreateTopic(ctx, common.TOPIC_NAME)
+	if err != nil {
+		fmt.Errorf("Failed to create topic: %v", err)
+	}
+
+	data, err := json.Marshal(bookingInfo)
+	if err != nil {
+		fmt.Errorf("Failed to marshal booking info: %v", err)
+	}
+
+	res, err := topic.Publish(ctx, &pubsub.Message{
+		Data:data,
+		Attributes: map[string]string{
+			common.EVENT_TYPE_ATTRIBUTE_NAME: common.BOOKING_EVENT_TYPE,
+			common.TIMESTAMP_ATTRIBUTE_NAME:  fmt.Sprintf("%d", time.Now().UnixNano()),
+		},
+	})
+
+	if err != nil {
+		return err.Error()
+	} else {
+		return strings.Join(res, ",")
 	}
 }
